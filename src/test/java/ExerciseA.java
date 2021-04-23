d5aed16a5db41bbde196c247a955b58bc5782dcd
MaNicolas <48954140+MaNicolas@users.noreply.github.com>
2021-04-23 15:48:14 +0200

    Initial commit

---
+++ src/test/java/ExerciseA.java
@@ -0,0 +1,15 @@
+import java.util.Map;
+
+import org.testng.annotations.Test;
+
+import com.automationpractice.base.CsvDataProviders;
+import com.automationpractice.base.TestUtilities;
+
+public class ExerciseA extends TestUtilities{
+
+	@Test(invocationCount = 1, dataProvider = "csvReader", dataProviderClass = CsvDataProviders.class)
+	public void exercise(Map<String, String> testData) {
+		
+		
+	}
+}
